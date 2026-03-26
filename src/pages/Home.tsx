0b3b1262ad0d13b04c4e3359b305934967388858
jordanjade900 <101015554+jordanjade900@users.noreply.github.com>
2026-03-26 16:13:07 -0500

    feat: Update logo image URL
    Replaces the old logo image URL with a new one hosted on Cloudinary, ensuring the correct and up-to-date branding is displayed on the homepage.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ import { RevealOverlay } from '../components/RevealOverlay';
 import { ElementModal, ElementType } from '../components/ElementModal';
 import { ImageModal } from '../components/ImageModal';
 
-const logo = 'https://i.ibb.co/LhRtjQT6/miss-utech-logo.png';
+const logo = 'https://res.cloudinary.com/dad155oxi/image/upload/v1774559377/miss-utech-logo_oy78sf.jpg';
 const elementsGroup = '/group-photo-elements.png';
 const fireDress = '/fire-dress.png';
 const earthDress = '/earth-dress.png';
